6a8ca688bbcc4dc87fd55b6ec244e9c8f1588a7c
Kate Miholenko <katemixa92@gmail.com>
2022-06-27 21:35:53 +0300

    refactor: added before and after annotations

--- src/test/java/DelivioTest.java
+++ src/test/java/DelivioTest.java
@@ -1,16 +1,30 @@
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DelivioTest {
+    public ChromeDriver webDriver;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webserver.chrome.driver", "C:\\Programs\\chromedriver.exe");
+        webDriver = new ChromeDriver();
+        System.out.println("WebDriver start");
+    }
+
     @Test
     public void openSite() {
-        System.setProperty("webserver.chrome.driver", "C:\\Programs\\chromedriver.exe");
-        ChromeDriver webDriver = new ChromeDriver();
         webDriver.get("https://delivio.by/");
         String title = webDriver.getTitle();
-        Assert.assertEquals("Деливио",title);
+        Assert.assertEquals("Деливио", title);
+    }
+
+    @After
+    public void closeWebDriver() {
         webDriver.quit();
+        System.out.println("WebDriver close");
     }
 
 }
