1cda0cb748e36699e168c413677aad014ce0d496
Kate Miholenko <katemixa92@gmail.com>
2022-06-27 21:42:23 +0300

    refactor: delete annotations before and after from DelivioTest class

--- src/test/java/DelivioTest.java
+++ src/test/java/DelivioTest.java
@@ -1,30 +1,13 @@
-import org.junit.After;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DelivioTest {
-    public ChromeDriver webDriver;
-
-    @Before
-    public void setUp() {
-        System.setProperty("webserver.chrome.driver", "C:\\Programs\\chromedriver.exe");
-        webDriver = new ChromeDriver();
-        System.out.println("WebDriver start");
-    }
+public class DelivioTest extends WebDriverSettings {
 
     @Test
     public void openSite() {
         webDriver.get("https://delivio.by/");
         String title = webDriver.getTitle();
         Assert.assertEquals("Деливио", title);
-    }
-
-    @After
-    public void closeWebDriver() {
-        webDriver.quit();
-        System.out.println("WebDriver close");
     }
 
 }
